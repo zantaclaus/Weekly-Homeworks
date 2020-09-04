7a19df404ad7f735850f9b79fb835cfca6d7fb95
Tarzuto <tarzuto@gmail.com>
2020-09-04 22:09:04 +0700

    Matrix Transpose

---
+++ Week4-HW1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int N, Matrix[100][100];
+	int temp;
+	
+	cin >> N;
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			cin >> Matrix[i][j];
+		}
+	}
+		
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			if(i < j)
+			{
+				temp = Matrix[i][j];
+				Matrix[i][j] = Matrix[j][i];
+				Matrix[j][i] = temp;
+			}
+		}
+		printf("\n");
+	}
+	
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			cout << Matrix[i][j] << " ";
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
