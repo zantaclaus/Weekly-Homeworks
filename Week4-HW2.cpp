c15fa241d2e1cf282273ebff8602b351e9eb6d4d
Tarzuto <tarzuto@gmail.com>
2020-09-04 23:03:47 +0700

    Bubble Sort Ascending Order

---
+++ Week4-HW2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int N, Num[500];
+	int temp;
+	cin >> N;
+	for(int i = 0; i < N; i++)
+	{
+		scanf("%d",&Num[i]);
+	}
+	
+	for(int i = 0; i < N-1; i++)
+	{
+		for(int j = 0; j < N-1; j++)
+		{
+			if(Num[j] > Num[j+1])
+			{
+				temp = Num[j];
+				Num[j] = Num[j+1];
+				Num[j+1] = temp;
+			}
+		}
+	}
+		
+	for(int i = 0; i < N; i++)
+	{
+		printf("%d ", Num[i]);
+	}
+	
+	return 0;
+}
