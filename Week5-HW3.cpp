57305e085c92c96ec3c96d799f306f3679f90793
Tarzuto <tarzuto@gmail.com>
2020-09-07 16:52:47 +0700

    Grader-NumberStairsRev

---
+++ Week5-HW3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int N, A[100];
+	cin >> N;
+	for(int i=N-1;i>=0;i--)
+	{
+		cin >> A[i];
+	}
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			cout << A[i];
+		}
+		cout << endl;
+	}
+	return 0;
+}
