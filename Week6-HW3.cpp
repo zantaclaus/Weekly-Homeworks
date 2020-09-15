84cce68ff7032de90e1de00b68b21eb4f5e19d71
Tarzuto <tarzuto@gmail.com>
2020-09-15 12:32:40 +0700

    Grader-2DStride1

---
+++ Week6-HW3.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int N, temp;
+	int str[100], code[100];
+	cin >> N;
+	for(int i=1;i<=N;i++)
+	{
+		if(i<100)
+		{
+			str[i] = i%10;
+			code[i] = i%10;
+		}
+		else
+		{
+			str[i] = i%100;
+			code[i] = i%100;
+		}
+	}
+	for(int i=1;i<=N;i++)
+	{
+		for(int j=1;j<=N;j++)
+		{
+			cout << code[j] << " ";
+		}
+		for(int k=1;k<=N;k++)
+		{
+			if(k == 1)
+			{
+				code[k] = str[N];
+			}
+			else code[k] = str[k-1];
+		}
+		for(int j=1;j<=N;j++)
+		{
+			str[j] = code[j];
+		}
+		cout << endl;
+	}
+	return 0;
+}
